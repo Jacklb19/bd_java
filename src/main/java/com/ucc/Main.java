6267c21a23cb6fb105b3bab9de68307ac32746f7
Jose Luis Burbano <jose.burbanob@campusucc.edu.co>
2025-10-21 08:58:36 -0500

    save method

--- src/main/java/com/ucc/Main.java
+++ src/main/java/com/ucc/Main.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import com.ucc.Connection.DatabaseConnection;
+import com.ucc.Model.Actor;
 import com.ucc.Repository.ActorRepository;
 import com.ucc.Repository.IRepository;
 
@@ -15,8 +16,14 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         try(Connection myConn = DatabaseConnection.getInstanceConnection()){
 
+            Actor actor = new Actor();
+            actor.setFirst_name("null");
+            actor.setLast_name("null");
+
             IRepository repository = new ActorRepository();
+            repository.save(actor);
             repository.findAll().forEach(System.out::println);
+            
 
         } catch (Exception e) {
             System.out.println("Conexion Fail");
